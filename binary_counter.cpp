77d5c27217d2548cba747139fa5bfe1e250bb615
DM <167101844+Tamil1Movie@users.noreply.github.com>
2025-01-08 14:53:45 +0530

    Add files via upload

---
+++ binary_counter.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int size=3;
+	int bits[3]={0};
+	for(int j=size-1;j>=0;j--)
+	{
+		cout<<bits[j];
+	}
+	cout<<endl;
+	for(int i=0;i<7;i++){
+		int carry=1;
+	for(int j=0;j<size&& carry==1;j++)
+	{
+		if(bits[j]==0)
+		{
+			bits[j]=1;
+			carry=0;
+		}
+		else
+		{
+			bits[j]=0;
+			carry=1;
+		}
+	}
+	for(int j=size-1;j>=0;j--)
+	{
+		cout<<bits[j];
+	}
+	cout<<endl;
+	}
+	return 0;
+}
